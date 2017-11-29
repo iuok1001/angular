c88fcf191e43976c3eef02a4bfdf80682ba08f1e
zhiliang.liu <zhiliang.liu@qishon.com>
2017-11-29 08:08:35 +0800

    lowercase and uppercase sample

--- sample/function/base/angular.uppercase.js
+++ sample/function/base/angular.uppercase.js
@@ -3,10 +3,10 @@ app.controller('MyCtrl', function () {
     var vm = this;
     //监听页面上的姓名，并改变
     //Deprecated: (since 1.5.0) (to be removed in 1.7.0)
-    //Use String.prototype.toLowerCase instead.
-    vm.toLowercase = function () {
-        vm.lowerName = angular.lowercase(vm.name);
-        vm.prototypeName = vm.name.toLowerCase();
+    //Use String.prototype.toUpperCase instead.
+    vm.toUppercase = function () {
+        vm.uppercaseName = angular.uppercase(vm.name);
+        vm.prototypeName = vm.name.toUpperCase();
     };
 
 });
