1aacd3ff3e24b346ebc1dd5f739956ca89a3e72d
zhiliang.liu <zhiliang.liu@qishon.com>
2017-12-03 10:56:14 +0800

    bootstrap

--- sample/function/base/angular.bootstrap.js
+++ sample/function/base/angular.bootstrap.js
@@ -1,7 +1,16 @@
 var app = angular.module('ngFunction', []);
 app.controller('MyCtrl', function ($scope) {
+    var vm = this;
     $scope.name = "hello,World!";
-    $scope.name2 = "hello,World2!";
+    vm.name2 = "hello,World222!";
 });
-
-angular.bootstrap(document, ['ngFunction']);
+var app2 = angular.module('ngFunction2', []);
+app2.controller('MyCtrl2', function ($scope) {
+    var vm = this;
+    $scope.name = "222hello,World!";
+    vm.name2 = "222hello,World222!";
+});
+angular.element(document).ready(function() {
+    angular.bootstrap(document.getElementById('div1'), ['ngFunction']);
+    angular.bootstrap(document.getElementById('div2'), ['ngFunction2']);
+});
